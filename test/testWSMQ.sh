7eda44e58b0c012a24876904ef35a41aaa07fc99
h3nc4 <me@h3nc4.com>
2024-04-26 19:21:19 -0300

    dev: testes #23

---
+++ test/testWSMQ.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Open the first instance of the WebSocket client
+xdg-open test/client/index.html
+sleep 1
+
+# Modify the WebSocket client to connect to port 3001
+sed -i 's/3000/3001/g' test/client/index.html
+
+# Open the second instance of the WebSocket client
+xdg-open test/client/index.html
+sleep 1
+
+# Modify the WebSocket client to connect back to port 3000
+sed -i 's/3001/3000/g' test/client/index.html
+
+# Change directory to the guard service
+cd code/guard
+
+# Start the first instance of the WebSocket server and capture its PID
+src/index.js &
+sleep 1
+pid1=$!
+
+# Modify the configuration file to listen on port 3001
+sed -i 's/3000/3001/g' src/.config.js
+
+# Start the second instance of the WebSocket server and capture its PID
+src/index.js &
+sleep 1
+pid2=$!
+
+# Modify the configuration file to listen back on port 3000
+sed -i 's/3001/3000/g' src/.config.js
+
+# Print the PIDs of the started processes
+echo "WebSocket server 1 on 3000: $pid1"
+echo "WebSocket server 2 on 3001: $pid2"

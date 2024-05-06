fe27d994f9d67d23b77b56fddbfedb7b055166e2
h3nc4 <me@h3nc4.com>
2024-05-06 20:11:34 -0300

    dev: navegação

--- code/client/App.tsx
+++ code/client/App.tsx
@@ -1,68 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, View, Text, Image, TextInput, RefreshControl } from 'react-native';
-import Header from './components/Header';
-import styles from './styles/styles';
-import { fetchData } from './utils/dataFetching';
-import { Item } from './types/Item';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import Home from './routes/Home';
+import Signup from './routes/Signup';
+import Login from './routes/Login';
+import Dashboard from './routes/Dashboard';
 
-export default function App() {
-  const [items, setItems] = useState<Array<Item>>([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [refreshing, setRefreshing] = useState(false);
+const Stack = createNativeStackNavigator();
 
-  useEffect(() => {
-    fetchData().then(data => setItems(data));
-  }, []);
-
-  const handleSearch = () => setItems(items.filter(item => `${item.name} ${item.description}`.toLowerCase().includes(searchQuery.toLowerCase())));
-
-  const onRefresh = async () => {
-    setRefreshing(true);
-    await fetchData().then(data => setItems(data));
-    setRefreshing(false);
-  };
-
-  const renderItem = ({ item }: { item: Item }) => {
-    return (
-      <View style={styles.item}>
-        <Image
-          style={styles.image}
-          source={{ uri: `data:image/jpeg;base64,${item.image}` }}
-        />
-        <Text style={styles.title}>{item.name}</Text>
-        <Text>{item.description}</Text>
-        <View style={{ marginTop: 10 }}>
-          <Text>Price: {item.price}</Text>
-          <Text>Vendor: {item.vendor}</Text>
-          <Text>Date: {item.date}</Text>
-        </View>
-      </View>
-    );
-  };
-
+function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchInput}
-          onChangeText={setSearchQuery}
-          value={searchQuery}
-          placeholder="Search items..."
-          onSubmitEditing={handleSearch}
-        />
-      </View>
-      <FlatList
-        data={items}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />
-        }
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
---
+++ code/client/routes/Dashboard.tsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { FlatList, View, Text, Image, TextInput, RefreshControl, StyleSheet } from 'react-native';
+import Header from '../components/Header';
+import { fetchData } from '../utils/dataFetching';
+import { Item } from '../types/Item';
+
+export default function Dashboard() {
+  const [items, setItems] = useState<Array<Item>>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => { fetchData().then(data => setItems(data)); }, []);
+
+  const handleSearch = () => setItems(items.filter(item => `${item.name} ${item.description}`.toLowerCase().includes(searchQuery.toLowerCase())));
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchData().then(data => setItems(data));
+    setRefreshing(false);
+  };
+
+  const renderItem = ({ item }: { item: Item }) => {
+    return (
+      <View style={styles.item}>
+        <Image
+          style={styles.image}
+          source={{ uri: `data:image/jpeg;base64,${item.image}` }}
+        />
+        <Text style={styles.title}>{item.name}</Text>
+        <Text>{item.description}</Text>
+        <View style={{ marginTop: 10 }}>
+          <Text>Price: {item.price}</Text>
+          <Text>Vendor: {item.vendor}</Text>
+          <Text>Date: {item.date}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          placeholder="Search items..."
+          onSubmitEditing={handleSearch}
+        />
+      </View>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 45,
+    maxWidth: 500,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  image: {
+    width: '70%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+  searchContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginBottom: 8,
+    borderRadius: 8,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+  },
+});

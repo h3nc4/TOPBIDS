c60736dd2ed4dfd7d31e8fdd7044fb847e5f0c2d
Renato-Paganini-Thurler <paganini.xxi@gmail.com>
2024-05-10 17:03:33 -0300

    dev: login de user #10

---
+++ code/client/routes/Login.tsx
@@ -0,0 +1,87 @@
+/*
+*  Copyright 2024 TopBids
+* 
+* This file is part of TopBids.
+* 
+* TopBids is free software: you can redistribute it and/or modify it
+* under the terms of the GNU Affero General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any
+* later version.
+* 
+* TopBids is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+* General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero
+* General Public License along with TopBids. If not, see
+* <https://www.gnu.org/licenses/>.
+*/
+
+import React, { useState } from 'react';
+import { Button, View, Text, TextInput, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { loginUser } from '../api/userApi';
+import Header from '../components/Header';
+
+interface LoginProps {
+    navigation: NativeStackScreenProps<any, 'Login'>['navigation'];
+}
+
+const Login = ({ navigation }: LoginProps) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            await AsyncStorage.setItem('user', await loginUser(username, password));
+            navigation.replace('Dashboard'); // Navigate to Dashboard
+        } catch (error) {
+            console.error('Error logging in:', error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Header />
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+});
+
+export default Login;

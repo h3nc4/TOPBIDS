87658d8c46feb2a6f167501154a7a3c650694cf3
h3nc4 <me@h3nc4.com>
2024-06-13 19:32:01 -0300

    dev: emails mobile

--- code/core/logic/api/auth.py
+++ code/core/logic/api/auth.py
@@ -17,10 +17,14 @@
 # <https://www.gnu.org/licenses/>.
 
 from ..models import *
-from ..utils import generate_token, decode_token
+from ..utils import generate_token, decode_token, mail, account_activation_token
 from .schemas import *
-from django.http import JsonResponse
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
 from ninja import Router
 import json
@@ -35,12 +39,23 @@ def signup(request):
     try:
         User.objects.create(username=inpt['username'],  email=inpt['email'],  password=make_password(inpt['password']),  cpf=inpt['cpf'],
                             phone=inpt['phone'],  address=inpt['address'],  city=inpt['city'],  state=inpt['state'],  zip_code=inpt['zip_code'], buyer=Buyer.objects.create())
-    except Exception:
-         # print information about the error
-         print("Error: ", Exception)
-         return JsonResponse({"status": "nok"}, status=400)
-    user = User.objects.get(username=inpt['username'])
-    return JsonResponse({"token": generate_token(user.id), "user": user.username})
+    except Exception as e:
+        # print information about the error
+        print("Error: ", e)
+        return JsonResponse({"status": "nok", "error": str(e)}, status=400)
+    if not Configs.objects.first().emails:
+        user = User.objects.get(username=inpt['username'])
+        return JsonResponse({"token": generate_token(user.id), "user": user.username})
+    user.is_active = False
+    user.save()
+    mail("Ative sua conta", "email/ativacao.html", {
+        'user': user.username,
+        "protocolo": 'https' if request.is_secure() else 'http',
+        'dominio': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+    }, user.email)
+    return JsonResponse({"status": "ok"})
 
 
 @auth_router.post("/login/")
@@ -48,18 +63,36 @@ def user_login(request):
     inpt = json.loads(request.body.decode("utf-8")).get('user', [])
     user = authenticate(request, username=inpt['username'], password=inpt['password'])
     if user:
+        if not user.is_active:
+            return JsonResponse({"error": "Account not activated"}, status=401)
         return JsonResponse({"token": generate_token(user.id), "user": user.username})
     return JsonResponse({"error": "Invalid credentials"}, status=401)
 
 
+@auth_router.get("/forgot/")
+def forgot_password(request):
+    email = json.loads(request.body.decode("utf-8")).get('email', [])
+    user = User.objects.get(email=email)
+    if user:
+        mail("Recuperação de senha", "email/recuperacao.html", {
+            'user': user.username,
+            "protocolo": 'https' if request.is_secure() else 'http',
+            'dominio': get_current_site(request).domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        }, user.email)
+        return JsonResponse({"message": "Email sent"})
+    return JsonResponse({"error": "Invalid email"}, status=401)
+
+
 @auth_router.post("/check/")
-def check_token(request): # receices token and returns if it is valid
+def check_token(request): # receives token and returns if it is valid
     try:
         body = json.loads(request.body.decode("utf-8"))
         user_id = decode_token(body.get('token', []))
         username = body.get('user', [])
         user = User.objects.get(id=user_id)
-        if user.username == username:
+        if user.username == username and user.is_active:
             return JsonResponse({"user_id": user_id, "status": "ok"})
         return JsonResponse({"status": "nok"}, status=401)
     except Exception:
@@ -83,7 +116,6 @@ def update_account(request):
         inpt = json.loads(request.body.decode("utf-8")).get('user', [])
         user.username = inpt['username'] if inpt['username'] else user.username
         user.email = inpt['email'] if inpt['email'] else user.email
-        user.password = make_password(inpt['password']) if inpt['password'] else user.password
         user.cpf = inpt['cpf'] if inpt['cpf'] else user.cpf
         user.phone = inpt['phone'] if inpt['phone'] else user.phone
         user.address = inpt['address'] if inpt['address'] else user.address

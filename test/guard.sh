bb9e8a0c39ecfa126c608c5d5cf07a0f39eae57f
h3nc4 <me@h3nc4.com>
2024-06-27 20:39:09 -0300

    test: 500 clientes

--- test/guard.sh
+++ test/guard.sh
@@ -1,39 +1,23 @@
 #!/usr/bin/env bash
 
-# Open the first instance of the WebSocket client
-xdg-open test/client/index.html
-sleep 1
+# Number of instances to open
+NUM_INSTANCES=500
 
-# Modify the WebSocket client to connect to port 3001
-sed -i 's/3000/3001/g' test/client/index.html
-
-# Open the second instance of the WebSocket client
-xdg-open test/client/index.html
-sleep 1
-
-# Modify the WebSocket client to connect back to port 3000
-sed -i 's/3001/3000/g' test/client/index.html
+# Loop to open WebSocket client instances
+for ((i=1; i<=NUM_INSTANCES; i++)); do
+    xdg-open test/client/index.html &
+    sleep 1
+done
 
 # Change directory to the guard service
 cd code/guard
 
-# Start the first instance of the WebSocket server and capture its PID
+# Start the instance of the WebSocket server and capture its PID
 node --env-file=.env ./src/index.js &
 pid1=$!
 sleep 1
 
-# Modify the configuration file to listen on port 3001
-sed -i 's/3000/3001/g' src/.env
-
-# Start the second instance of the WebSocket server and capture its PID
-node --env-file=.env ./src/index.js &
-pid2=$!
-sleep 1
-
-# Modify the configuration file to listen back on port 3000
-sed -i 's/3001/3000/g' src/.env
-
-# Print the PIDs of the started processes
+# Print the PID of the started WebSocket server process
 echo "WebSocket server 1 on 3000: $pid1"
-echo "WebSocket server 2 on 3001: $pid2"
-echo "kill $pid1 $pid2"
+echo "To kill the WebSocket server:"
+echo "kill $pid1"
